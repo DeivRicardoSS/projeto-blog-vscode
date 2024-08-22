b1371ff021b8d1629ef5197871280dafac9dcd15
Deivyson <131066052+DeivRicardoSS@users.noreply.github.com>
2024-08-22 18:19:48 -0300

    add user schema to database

---
+++ back_end/src/database/posts.database.ts
@@ -0,0 +1,39 @@
+// Importações
+import mongoose from 'mongoose';
+
+const PostSchema = new mongoose.Schema({
+    id: {
+        type: String,
+        required: true
+    },
+    id_dono:{
+        type: String,
+        required: true
+    },
+    cor:{
+        type: [String],
+        required: true
+    },
+    titulo:{
+        type: String,
+        required: true
+    },
+    subtitulo:{
+        type: String,
+        required: true
+    },
+    conteudo:{
+        type: String,
+        required: true
+    },
+    categorias: {
+        type: String,
+        required: true
+    },
+    views: {
+        type: Number,
+        required: true
+    }
+})
+
+export default PostSchema;
