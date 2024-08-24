e357133bba9f47004c21d17ab094f77f536d2eaf
Deivyson <131066052+DeivRicardoSS@users.noreply.github.com>
2024-08-23 21:30:20 -0300

    update

---
+++ back_end/src/database/respostas.database.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const RespostaSchema = new mongoose.Schema({
+    id: {
+        type: String,
+        required: true
+    },
+    id_pai: {
+        
+    },
+    id_dono: {
+
+    },
+    conteudo: {
+
+    }
+})
